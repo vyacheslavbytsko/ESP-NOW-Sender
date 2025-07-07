df13544faed5e1531be564800eb559cf98c088d2
Elur12 <m.klyshkin@ya.ru>
2025-07-07 21:11:27 +0300

    new

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,11 @@
 #include <WiFi.h>
 #include <esp_now.h>
+#include <Arduino.h>
 
 uint8_t receiverMac[] = {0x74, 0x4D, 0xBD, 0x8A, 0x07, 0x64}; // ESP32 S3
 //uint8_t receiverMac[] = {0x28, 0x37, 0x2F, 0x6A, 0xC0, 0xA4} // ESP32 C3 SuperMini
+
+unsigned long i = 0;
 
 void onSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
     Serial.print("Send Status: ");
@@ -35,10 +38,11 @@ void setup() {
 }
 
 void loop() {
-    const char *msg = "Hello ESP-NOW!";
+    std::string msgStr = std::to_string(i++);
+    const char *msg = msgStr.c_str();
     esp_err_t result = esp_now_send(receiverMac, (uint8_t *)msg, strlen(msg) + 1);
     if (result != ESP_OK) {
         Serial.println("Send failed");
     }
-    delay(5);
+    delay(100);
 }
